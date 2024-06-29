b56003b4f87948f79499fe45552fcf626d5a91f0
Burak Kurt <krtburak@outlook.com>
2024-06-29 04:11:40 +0300

    add loading

--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Helmet from "../components/Helmet";
 import { getNotes } from "../services/notes";
 import NoteCard from "../components/NoteCard";
@@ -9,18 +9,22 @@ import { useAuthContext } from "../hooks/useAuthContext";
 
 function Notes() {
   const { notes, dispatch } = useNotContext();
+  const [loading, setLoading] = useState(false);
   const { user } = useAuthContext();
 
   useEffect(() => {
-    user &&
-      getNotes(user?.token).then((data) =>
-        dispatch({ type: "FILL_NOTE", payload: data })
-      );
+    if (user) {
+      setLoading(true);
+      getNotes(user?.token)
+        .then((data) => dispatch({ type: "FILL_NOTE", payload: data }))
+        .finally(() => setLoading(false));
+    }
   }, [dispatch, user]);
 
   return (
     <div>
       <Helmet title="Notes" />
+
       <Link
         to="add"
         className="flex flex-row items-center gap-1 text-lg bg-slate-100 py-1.5 px-3 w-fit rounded-lg shadow-md mb-5 ms-auto"
@@ -28,11 +32,19 @@ function Notes() {
         <IoMdAddCircle className="text-xl" />
         Add Note
       </Link>
-      <div className="grid grid-cols-3 gap-5">
-        {notes &&
-          notes.length > 0 &&
-          notes.map((note, index) => <NoteCard data={note} key={index} />)}
-      </div>
+      {!loading ? (
+        <>
+          <div className="grid grid-cols-3 gap-5">
+            {notes &&
+              notes.length > 0 &&
+              notes.map((note, index) => <NoteCard data={note} key={index} />)}
+          </div>
+        </>
+      ) : (
+        <div className="flex items-center justify-center h-full min-h-[400px]">
+          <h4 className="text-2xl text-slate-400">Loading...</h4>
+        </div>
+      )}
     </div>
   );
 }
